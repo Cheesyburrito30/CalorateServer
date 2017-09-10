8982032ca3b63014708c906bd6c6ff8f795168f2
Vernon Perry <pvernon30@gmail.com>
2017-09-10 05:12:58 -0400

    server

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize(process.env.DATABASE_URL ||
-	'postgres://postgres:VlpCartel111@localhost:5432/workoutlog', {
-		dialect: 'postgres'
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+		logging: false,
+		dialectOptions: { ssl : true }
 	})
 
 sequelize.authenticate().then(
